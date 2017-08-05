87cfa962bbdeb79f00f304aa530b6c2918d0e05c
dima <dbabits@gmail.com>
2017-08-05 02:30:27 -0400

    working version

---
+++ replace_xs/decimal_to_binary.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>     /* atoi */
+#include <vector>
+#include <cmath>
+
+int main (int argc, char* argv[] ){
+
+   int input=(argc>1) ? atoi(argv[1]) : std::pow(2,3);
+
+   std::vector<int> bits;
+   if (input==0) {
+	  bits.push_back(0);
+   }
+   else {
+      for (int i=input;i>0;i/=2){
+		bits.push_back(i % 2);
+	  }
+   }
+   
+   printf("input=%d, in binary=", input);
+   for (std::vector<int>::reverse_iterator rit = bits.rbegin();rit!=bits.rend(); ++rit){
+      printf("%d",*rit);
+   }
+   printf("\n");
+
+   return 0;
+}
