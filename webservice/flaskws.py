9c43cedbcb8d4ff2a6c2ad422b870678ab7e0414
dima <dbabits@gmail.com>
2017-08-05 18:18:20 -0400

    installed pip,flask, added comments to flaskws

--- webservice/flaskws.py
+++ webservice/flaskws.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+'''
+ubuntu:
+ sudo apt install python-pip
+ dima@LAPTOP-MA6OEPO9:~/development/pxs_coding_challenge/webservice$ pip install flask
+ dima@LAPTOP-MA6OEPO9:~/development/pxs_coding_challenge/webservice$ sudo pip install --upgrade pip
+ dima@LAPTOP-MA6OEPO9:~/development/pxs_coding_challenge/webservice$ ./flaskws.py
+ 
+ /home/dima/.cache
+ 
+ >>> import hashlib, binascii
+>>> dk = hashlib.pbkdf2_hmac('sha256', b'password', b'salt', 100000)
+>>> binascii.hexlify(dk)
+b'0394a2ede332c9a13eb82e9b24631604c31df978b4e2f0fbd2c549944f9d79a5'
+
+'''
 from flask import Flask
 app = Flask(__name__)
  
