37a2b430a16442580dfdd83387e7f03ecd6fdb76
dima <dbabits@gmail.com>
2017-08-06 14:51:46 -0400

    gift card-added logging and arg parsing

--- gift_card/try1.py
+++ gift_card/try1.py
@@ -1,64 +1,78 @@
 #!/usr/bin/env python
+import logging
+import argparse
 
 def name(g):
-	return g[0]
+    return g[0]
 def price (g):
-	return g[1]
-	
+    return g[1]
+
 gifts=( 
-		 ("bar",125,125)
-		,("moo",125,125)
-		,("foo",250,250)
+         ("bar",125,125)
+        ,("moo",125,125)
+        ,("foo",250,250)
         ,("candy bar", 500,500)
-		,("paperback book", 700,700)
-		,("detergent",1000,1000)
-		,("headphones",1400,1400)
-		,("earmuffs",2000,2000)
-		,("bluetooth stereo",6000,6000)
-	)
+        ,("paperback book", 700,700)
+        ,("detergent",1000,1000)
+        ,("headphones",1400,1400)
+        ,("earmuffs",2000,2000)
+        ,("bluetooth stereo",6000,6000)
+    )
 
 '''
 for each item, iterate over all other items, starting from the next (e.g.: do not iterate both a,b and b,a - only one combo. Data assumed to be sorted
 '''
 def find_pair(total):
-	#for i, item in enumerate(gifts):
-	best_diff=None
-	iters=0
-	print "--------find_pair(",total,")"
-	for i in range (0,len(gifts)):
-		print i,"-",gifts[i];
-		if (price(gifts[i]) >=total): #then no second item is possible and we can break here. It's just an optimization.
-			print "optimization1: breaking off because ", price(gifts[i]),">=total(",total,")"
-			break
-			
-		for j in range (i+1,len(gifts)):
-			s=gifts[i][1] + gifts [j][1]
-			diff=total-s
-			if diff>=0 and (diff < best_diff or best_diff is None):
-				best_diff=diff
-				item1=i
-				item2=j
-				if diff==0:
-					print "optimization2:breaking off because diff=0:perfect match, no need to look further"
-					break 
-			elif diff<0:
-					#print "optimization3:breaking off because diff(",diff,")<0(sum>total):since data is sorted,no need to look further"
-					#break 	
-					pass
-			elif diff>=best_diff:
-					#print "optimization4:breaking off because diff(",diff,")>=best_diff(",best_diff,"):since data is sorted,no need to look further"
-					#break 		
-					pass
-			print "  and ",j, "--",gifts[j], "total=",total,"--sum=",s, "--diff=",(total-s), "--best diff=",best_diff
-			iters+=1
-	
-	if best_diff is None:
-		print "find_pair(",total,")-Best combination Not possible"," nItems=",len(gifts)," Iterations=",iters
-		return []
-	else:
-		print "find_pair(",total,")-Best combination=",gifts[item1],",", gifts[item2], "sum=",gifts[item1][1]+gifts[item2][1]," best_diff=",best_diff," nItems=",len(gifts)," Iterations=",iters
-		return [gifts[item1],gifts[item2]]
-		
+
+    best_diff=None
+    iters=0
+    logging.debug("--------find_pair(%d)",total)
+    #for i, item in enumerate(gifts):
+    for i in range (0,len(gifts)):
+        logging.debug("%d - %s",i,gifts[i])
+        if (price(gifts[i]) >=total): #then no second item is possible and we can break here. It's just an optimization.
+            logging.debug("optimization1: breaking off because %d>=total(%d)", price(gifts[i]),total )
+            break
+
+        for j in range (i+1,len(gifts)):
+            s=gifts[i][1] + gifts [j][1]
+            diff=total-s
+            if diff>=0 and (diff < best_diff or best_diff is None):
+                best_diff=diff
+                item1=i
+                item2=j
+                if diff==0:
+                    logging.debug( "optimization2:breaking off because diff=0:perfect match, no need to look further")
+                    break
+            elif diff<0:
+                    #print "optimization3:breaking off because diff(",diff,")<0(sum>total):since data is sorted,no need to look further"
+                    #break
+                    pass
+            elif diff>=best_diff:
+                    #print "optimization4:breaking off because diff(",diff,")>=best_diff(",best_diff,"):since data is sorted,no need to look further"
+                    #break
+                    pass
+            logging.debug(" and %d-- %s total=%d --sum=%d --diff=%d --best diff=%d",j,gifts[j],total,s,total-s,best_diff)
+            iters+=1
+
+    if best_diff is None:
+        print "find_pair(",total,")-Best combination Not possible"," nItems=",len(gifts)," Iterations=",iters
+        return []
+    else:
+        print "find_pair(",total,")-Best combination=",gifts[item1],",", gifts[item2], "sum=",gifts[item1][1]+gifts[item2][1]," best_diff=",best_diff," nItems=",len(gifts)," Iterations=",iters
+        return [gifts[item1],gifts[item2]]
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="make it verbose",action="store_true")
+
+args = parser.parse_args()
+loglevel=logging.INFO
+if args.verbose:
+    loglevel=logging.DEBUG
+
+#logging.basicConfig(format='%(levelname)s:%(message)s',level=loglevel)
+logging.basicConfig(format='%(message)s',level=loglevel)
 
 assert(find_pair (400)  ==[('bar', 125, 125) , ('foo', 250, 250)])
 assert(find_pair (2500) ==[("candy bar", 500,500),("earmuffs",2000,2000)])
