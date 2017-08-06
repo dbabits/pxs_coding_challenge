58ac10b6206f46ebbab6adb743efc7fb9e10e6dd
dima <dbabits@gmail.com>
2017-08-06 14:10:49 -0400

    misc binary_combos

--- replace_xs/binary_combinations.c
+++ replace_xs/binary_combinations.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-void print(const char* s, int n) {
 
+void print(const char* s, int n) {
+   static int counter=0;
+   printf("%d: ",++counter);
    for (int i = 0; i != n; i++) {
       printf("%c",  (s[i] == 'X' || s[i] == '0') ? '0' : '1' );
    }
    printf("\n");
 }
-
 
 int main(int argc, char* argv[]) {
    char d[] = "10X10X0";
