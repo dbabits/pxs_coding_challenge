c6d6ad3a22eb25b8fa8d3e47e30818b29db03dd3
dima <dbabits@gmail.com>
2017-08-06 00:50:31 -0400

    ws is implemented

--- webservice/flaskws.py
+++ webservice/flaskws.py
@@ -8,42 +8,56 @@ ubuntu:
  
  /home/dima/.cache
  
- >>> import hashlib, binascii
->>> dk = hashlib.pbkdf2_hmac('sha256', b'password', b'salt', 100000)
->>> binascii.hexlify(dk)
-b'0394a2ede332c9a13eb82e9b24631604c31df978b4e2f0fbd2c549944f9d79a5'
+dima@LAPTOP-MA6OEPO9:~$ curl -X POST -H "Content-Type: application/json" -d '{"message":"foo"}' http://127.0.0.1:5000/messages/
+{
+  "digest": "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
+}
 
-curl -X POST -H "Content-Type: application/json" -d '{"message":"foo"}' http://127.0.0.1:5000/messages/ 
-curl http://127.0.0.1:5000/messages/
+curl -i http://127.0.0.1:5000/messages/2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae
 
 '''
 
-from flask import Flask, request, abort, make_response
+from flask import Flask, request, abort, make_response,jsonify
+import flask
 import hashlib
 
 app = Flask(__name__)
 
+messages = {}
 
 @app.route('/', methods=["GET"])
 def index():
     return "Hello, World!"
 
+@app.route('/version/', methods=["GET"])
+def get_version():
+    return jsonify({"flask_version":flask.__version__})
 
-@app.route('/messages/', methods=['GET', 'POST'])
-def messages():
-    if request.method == 'POST':
-        #note = request.data.get('message', '')
-		request_json=request.get_json()
-		msg=request_json.get('message','message not provided')
+@app.route('/messages/', methods=['GET'])
+def get_messages():
+    return jsonify({'messages': messages})
 
-        # idx = max(notes.keys()) + 1
-        # notes[idx] = note
-        # return note_repr(idx), status.HTTP_201_CREATED
-		return msg
+@app.route('/messages/<string:digest>', methods=['GET'])
+def digest_to_message(digest):
+    msg=messages.get(digest)
+    if msg == None:
+        return jsonify({"err_msg": "Message not found"}), 404
+    else:
+        return jsonify({"message":msg})
 
-    hash = hashlib.sha256(b'foo')
-    hex = hash.hexdigest()
-    return hex
+@app.route('/messages/', methods=['POST'])
+def msg_to_digest():
+    assert(request.method == 'POST')
 
+    request_json=request.get_json()
+    #msg=request_json.get('message','message not provided')
+    msg = request_json.get('message')
+    if msg == None:
+        return jsonify({"err_msg": "message not passed.Usage:curl -X POST -H 'Content-Type: application/json' -d '{'message':'foo'}' "}), 404
+
+    hash = hashlib.sha256(msg)
+    digest = hash.hexdigest()
+    messages[digest] = msg
+    return jsonify({"digest":digest})
 
 app.run(debug=True)
