64661717e98d99eb53d2a527838961495f85b5f9
dima <dbabits@gmail.com>
2017-08-05 21:58:49 -0400

    dev

--- webservice/flaskws.py
+++ webservice/flaskws.py
@@ -13,14 +13,37 @@ ubuntu:
 >>> binascii.hexlify(dk)
 b'0394a2ede332c9a13eb82e9b24631604c31df978b4e2f0fbd2c549944f9d79a5'
 
+curl -X POST -H "Content-Type: application/json" -d '{"message":"foo"}' http://127.0.0.1:5000/messages/ 
+curl http://127.0.0.1:5000/messages/
+
 '''
-from flask import Flask
+
+from flask import Flask, request, abort, make_response
 import hashlib
+
 app = Flask(__name__)
- 
+
+
 @app.route('/', methods=["GET"])
 def index():
-    hash=hashlib.sha256(b'foo')
-    hex=hash.hexdigest()
-    return "Hello World. sha256(foo)="+hex
+    return "Hello, World!"
+
+
+@app.route('/messages/', methods=['GET', 'POST'])
+def messages():
+    if request.method == 'POST':
+        #note = request.data.get('message', '')
+		request_json=request.get_json()
+		msg=request_json.get('message','message not provided')
+
+        # idx = max(notes.keys()) + 1
+        # notes[idx] = note
+        # return note_repr(idx), status.HTTP_201_CREATED
+		return msg
+
+    hash = hashlib.sha256(b'foo')
+    hex = hash.hexdigest()
+    return hex
+
+
 app.run(debug=True)
